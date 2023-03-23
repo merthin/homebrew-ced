7e0b1f3b7ef0ccbd73b2b1b3bef7d8b90d7b7209
Luis <38664305+gprotel@users.noreply.github.com>
2023-03-23 11:00:18 -0400

    Update cli.rb

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,7 +1,7 @@
 class Cli < Formula
   desc "Merthin Clod/Edge Distribution Command Line Interface"
   homepage "https://merthin.com"
-  url "https://github.com/merthin/homebrew-brew/raw/main/src/ced-cli-latest.tgz"
+  url "https://github.com/merthin/homebrew-ced/raw/main/src/ced-cli-latest.tgz"
   version "1.1.69"
   sha256 "4efb7b2abc5772dc2eb78e0638fe652932ddf02dda5621c6d9635e8b29efab10"
 
