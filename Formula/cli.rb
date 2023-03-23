64850976558fca4601f7e5492ce79c68634b380b
Luis <38664305+gprotel@users.noreply.github.com>
2023-03-23 11:00:52 -0400

    Update cli.rb

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -3,7 +3,7 @@ class Cli < Formula
   homepage "https://merthin.com"
   url "https://github.com/merthin/homebrew-ced/raw/main/src/ced-cli-latest.tgz"
   version "1.1.69"
-  sha256 "4efb7b2abc5772dc2eb78e0638fe652932ddf02dda5621c6d9635e8b29efab10"
+  sha256 "d6d051d858dcfb4cfdc207452a835e49787fe407b98d463bc7173df84e153a8a"
 
   def install
     libexec.install Dir["*"]
