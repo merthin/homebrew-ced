d7622339418ab012554d5d38c608768b82ebcb60
CED Deploy Automation <ced@merthin.com>
2023-05-23 15:01:00 +0000

    Updating CLI version to 1.1.69

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -3,7 +3,7 @@ class Cli < Formula
   homepage "https://merthin.com"
   url "https://github.com/merthin/homebrew-ced/raw/main/src/ced-cli-latest.tgz"
   version "1.1.69"
-  sha256 "9be39e454c38b15cfec53f2ca5d05f7177e3216c2c1a8a11023288c18d291706"
+  sha256 "b44008a80354cf4987b17ad573646283aedae56ed232bb931749ab1b7614c6f3"
 
   def install
     libexec.install Dir["*"]
