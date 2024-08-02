25283bad4e14314796253b2c12ddc39080c6c7af
gprotel <luis@merthin.com>
2024-08-02 01:22:58 +0000

    updating ced cli version

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -2,8 +2,8 @@ class Cli < Formula
   desc "Merthin Cloud/Edge Distribution Command Line Interface"
   homepage "https://merthin.com"
   url "https://github.com/merthin/homebrew-ced/raw/main/src/ced-cli-latest.tgz"
-  version "1.1.70"
-  sha256 "383631a28a2e1c7fd24077099f30fe0166169819f46b7c8154f019ec68af546d"
+  version "1.1.79"
+  sha256 "8780829d67075c07d145d3620368a6a5b08dd92584ce8ee808fd7e722c11c7d1"
 
   def install
     libexec.install Dir["*"]
